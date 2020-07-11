473045beeb9d95406e02e1bce27940b2953c415a
Konstantin Podgorny <podgorny.kv@gmail.com>
2020-07-11 13:47:18 +0300

    1. GuessGame

---
+++ src/fhj/guessgame/GameLauncher.java
@@ -0,0 +1,8 @@
+package fhj.guessgame;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
---
+++ src/fhj/guessgame/Player.java
@@ -0,0 +1,9 @@
+package fhj.guessgame;
+
+public class Player {
+    int number = 0;
+    public void guess() {
+        number = (int) (Math.random() * 10);
+        System.out.println("Думаю, это число " + number);
+    }
+}
